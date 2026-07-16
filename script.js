1e53243eeab2ec26955ad5286d1571b26931898f
andrysvalencia316-ux <andrysvalencia316@gmail.com>
2026-07-16 09:49:54 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Sancocho - Dominican Food</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+    <h1>Sancocho</h1>
+
+    <p>Sancocho is a hearty Dominican stew made with different meats, vegetables, and root crops.</p>
+
+    <h2>Learn More</h2>
+
+    <ul>
+        <li><a href="https://www.dominicancooking.com/125/sancocho" target="_blank">Dominican Cooking - Sancocho</a></li>
+        <li><a href="https://en.wikipedia.org/wiki/Sancocho" target="_blank">Wikipedia - Sancocho</a></li>
+        <li><a href="https://www.tasteatlas.com/sancocho" target="_blank">TasteAtlas - Sancocho</a></li>
+    </ul>
+
+    <br>
+
+    <a href="index.html">Back to Home</a>
+
+</body>
+</html>
